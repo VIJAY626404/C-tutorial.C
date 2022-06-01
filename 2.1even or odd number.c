cd119bc030a360f929e8e2d28643ccb8ea0fdadb
Vijay Kumar Shah <97323848+VIJAY626404@users.noreply.github.com>
2022-06-01 20:50:48 +0530

    Create 2.1even or odd number.c

---
+++ 2.1even or odd number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int num;
+  printf("Enter the number is \n",num);
+  scanf("%d", &num);
+  if(num%2==0){
+    printf(" %d is even numbber\n",num);
+           }
+           else{
+             printf("%d is odd number\n", num);
+           }
+                    return 0;
+                    }
